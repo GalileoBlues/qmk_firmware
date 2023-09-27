cf275b8fcd9be422f28d363d51c1434a4c884972
Bryson James <105921721+GalileoBlues@users.noreply.github.com>
2023-09-27 20:45:40 +0930

    updated to reflect more sane combos on strain + derangement
    - changed backspace to a two handed combo as i'm so washed that i will probably strain my right hand if i keep using the current combo
    
    - also changed shift one-shot combo to something similar
    
    - changed to an index swapped Recurva to satisfy my derangement

--- keyboards/bastardkb/dilemma/3x5_2/keymaps/brys/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_2/keymaps/brys/keymap.c
@@ -22,6 +22,8 @@
 // when tapped produces capslock, otherwise acts as dead key for sym/misc layer (L3)
 #define L3_CAPS_DEAD LT(LAYER_SYM_MISC, KC_CAPS)
 
+#define SHIFT_ONE OSM(MOD_LSFT)
+
 enum dilemma_keymap_layers {
     LAYER_BASE = 0,
     LAYER_NUM_NAV,//layer for numbers, arrows and modifier keys
@@ -29,24 +31,24 @@ enum dilemma_keymap_layers {
 };
 
 // clang-format off
-/** \brief Recurva layout (3 rows, 10 columns). */
+/** \brief Recurva (colstag index swap) layout (3 rows, 10 columns). */
 #define LAYOUT_LAYER_BASE                                                                     \
-       KC_F,    KC_R,    KC_D,    KC_P,    KC_V,            KC_J,    KC_M,    KC_U,     KC_O,    KC_Y, \
-       KC_S,    KC_N,    KC_T,    KC_C,    KC_B,            KC_DOT,  KC_H,    KC_E,     KC_A,    KC_I, \
-       KC_Z,    KC_X,    KC_K,    KC_G,    KC_W,            KC_Q,    KC_L,    KC_SCLN,  KC_QUOT, KC_COMM, \
+       KC_F,    KC_R,    KC_D,    KC_L,    KC_J,            KC_V,    KC_W,    KC_U,     KC_O,    KC_Y, \
+       KC_S,    KC_N,    KC_T,    KC_H,    KC_DOT,          KC_B,    KC_C,    KC_E,     KC_A,    KC_I, \
+       KC_Z,    KC_X,    KC_K,    KC_M,    KC_Q,            KC_G,    KC_P,    KC_SCLN,  KC_QUOT, KC_COMM, \
                            L2_META_DEAD, KC_LCTL,           KC_SPC, L3_CAPS_DEAD
 
 #define LAYOUT_NUM_NAV                                                                      \
-       KC_ESC,  KC_1,      KC_2,      KC_3,    KC_4,         KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT, \
-       KC_TAB,  KC_COPY,   KC_PSTE,   KC_LALT, KC_5,         KC_6,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, \
-       KC_NO, LALT(KC_F3), LCA(KC_T), KC_PSCR, KC_NO,        KC_NO, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, \
-                               L2_META_DEAD, KC_LSFT,        KC_SPC, L3_CAPS_DEAD
+       KC_ESC,  KC_1,      KC_2,      KC_3,    KC_4,            KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT, \
+       KC_TAB,  KC_COPY,   KC_PSTE,   KC_LALT, KC_5,            KC_6,    KC_LEFT, KC_UP, KC_DOWN,   KC_RIGHT, \
+       KC_LEFT_GUI, LALT(KC_F3), LCA(KC_T), KC_PSCR, KC_NO,     KC_NO, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, \
+                               L2_META_DEAD, KC_LSFT,           KC_LSFT, L3_CAPS_DEAD
 
 #define LAYOUT_SYM_MISC                                                                             \
        KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_QUES,            KC_DLR, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, \
        KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_GRV,             KC_EXLM, KC_SLSH, KC_PAST, KC_EQL, KC_MINS, \
        KC_F9, KC_F10, KC_F11, KC_F12, KC_UNDS,            KC_CIRC, KC_RABK, KC_LABK, KC_AT, KC_PERC, \
-                        L2_META_DEAD, KC_LSFT,            KC_SPC, L3_CAPS_DEAD
+                        L2_META_DEAD, KC_LSFT,            KC_LSFT, L3_CAPS_DEAD
 
 
 #define LAYOUT_wrapper(...) LAYOUT_split_3x5_2(__VA_ARGS__)
@@ -59,10 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // adds combos for backspace (; + ,) and for enter (q + ')
 
-const uint16_t PROGMEM backspace_combo[] = {KC_M, KC_O, COMBO_END};
+const uint16_t PROGMEM backspace_combo[] = {KC_T, KC_E, COMBO_END};
 const uint16_t PROGMEM enter_combo[] = {KC_Q, KC_QUOT, COMBO_END};
-
-
+const uint16_t PROGMEM shift_combo[] = {KC_S, KC_I, COMBO_END};
 /*
 const uint16_t PROGMEM colon_combo[] = {KC_LCTL, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM doublequote_combo[] = {KC_LCTL, KC_QUOT, COMBO_END};
@@ -73,6 +74,7 @@ const uint16_t PROGMEM LANGLE_BRACKET_combo[] = {KC_LCTL, KC_DOT, COMBO_END};
 combo_t key_combos[] = {
     COMBO(backspace_combo, KC_BSPC),
     COMBO(enter_combo, KC_ENT),
+    COMBO(shift_combo, SHIFT_ONE),
     //COMBO(colon_combo, KC_COLN),
     //COMBO(doublequote_combo, KC_DQUO),
     //COMBO(RANGLE_BRACKET_combo, KC_RABK),
@@ -92,11 +94,13 @@ const key_override_t ctrl_semicolon_override = ko_make_basic(MOD_MASK_CTRL, KC_S
 const key_override_t ctrl_quote_override = ko_make_basic(MOD_MASK_CTRL, KC_QUOT, KC_DQUO);
 const key_override_t ctrl_comma_override = ko_make_basic(MOD_MASK_CTRL, KC_COMM, KC_RABK);
 const key_override_t ctrl_period_override = ko_make_basic(MOD_MASK_CTRL, KC_DOT, KC_LABK);
+//const key_override_t shift_override = ko_make_basic(KC_LCTL, KC_SPC, SHIFT_ONE);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &ctrl_semicolon_override,
     &ctrl_quote_override,
     &ctrl_comma_override,
     &ctrl_period_override,
+    //&shift_override,
     NULL
 };
